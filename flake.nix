93b6e7a164ca116c7f93163c79e592a3f66740a8
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-04-28 21:33:39 -0600

    Part one of migration to github repo.

---
+++ flake.nix
@@ -0,0 +1,108 @@
+{
+  description = "My system configs flake";
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    spicetify-nix = {
+      url = "github:the-argus/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, sops-nix, spicetify-nix, ... }@inputs:
+    let
+      inherit (self) outputs;
+
+      systems = [ "x86_64-linux" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      # Create a function that takes in a hostname, a list of usernames(strings) and extraSpecialArgs and returns a nixos system configuration
+      # Example usage: mkNixosSystem { hostname = "NixOSVM"; usernames = [ "joshk" "rick" ]; extraSpecialArgs = { }; }
+      mkNixosSystem = { hostname, usernames, extraSpecialArgs ? { } }:
+        let
+          inherit (nixpkgs) lib pkgs;
+
+          # Define a function to generate user configurations
+          mkUsers = usernames:
+            let
+              userConfig = username: ./homes/${username}/home.nix;
+            in
+            builtins.listToAttrs (map
+              (username: {
+                name = username;
+                value = userConfig username;
+              })
+              usernames);
+        in
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs outputs; } // extraSpecialArgs;
+          modules = [
+            ./systems/${hostname}/default.nix
+            ./systems/common/default.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.extraSpecialArgs = { inherit inputs outputs; } // extraSpecialArgs;
+              home-manager.useGlobalPkgs = true;
+              home-manager.users = mkUsers usernames;
+            }
+          ];
+        };
+
+    in
+    {
+      # NixOS modules. Like nixpkgs modules.
+      nixosModules = import ./modules/nixos;
+
+      # Home Manager modules.
+      homeModules = import ./modules/home-manager;
+
+      # Custom packages and modifications, exported as overlays
+      overlays = import ./overlays { inherit inputs outputs; };
+
+      # Custom packages. Accessible through 'nix build', 'nix shell', etc
+      packages = forAllSystems (pkgs: import ./pkgs { inherit pkgs; });
+
+      # Dev shells
+      devShell = forAllSystems (pkgs: import ./shell.nix { inherit pkgs; });
+
+      # Formatter for nix files, available through 'nix fmt'
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+
+      nixosConfigurations = {
+        NixOSVM = mkNixosSystem {
+          hostname = "NixOSVM";
+          usernames = [ "joshk" ];
+          extraSpecialArgs = { };
+        };
+        NixOSLaptop = mkNixosSystem {
+          hostname = "NixOSLaptop";
+          usernames = [ "joshk" ];
+          extraSpecialArgs = { };
+        };
+        NixOSDesktop = mkNixosSystem {
+          hostname = "NixOSDesktop";
+          usernames = [ "joshk" ];
+          extraSpecialArgs = { };
+        };
+      };
+    };
+}
+
+
