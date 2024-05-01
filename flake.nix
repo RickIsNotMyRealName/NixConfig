e2b44c6e83a826f8e20b79ab55665dd8f7c65e6e
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-04-30 21:45:03 -0600

    Added `ai.cachix` to flake.

--- flake.nix
+++ flake.nix
@@ -1,4 +1,9 @@
 {
+  nixConfig = {
+    extra-substituters = [ "https://ai.cachix.org" ];
+    extra-trusted-public-keys = [ "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc=" ];
+  };
+
   description = "My system configs flake";
   inputs = {
     nixpkgs = {
