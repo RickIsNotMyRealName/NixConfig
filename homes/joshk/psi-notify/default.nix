e9191a2e0b7dca7e6f137caa1ee057ce260e57bd
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-05-02 17:30:08 -0600

    Changed some values for psi-notify.

--- homes/joshk/psi-notify/default.nix
+++ homes/joshk/psi-notify/default.nix
@@ -3,10 +3,10 @@
   services.psi-notify = {
     enable = true;
     settings = {
-      update = "10";
+      update = "5";
       log_pressures = "false";
       threshold = {
-        cpu = "some avg10 50.00";
+        cpu = "some avg10 75.00";
         memory = "some avg10 10.00";
         io = "full avg10 15.00";
       };
