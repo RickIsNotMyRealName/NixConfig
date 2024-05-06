ea383f019223b2621297da6aa51bfd49286db51d
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-05-05 22:34:33 -0600

    Added mangohud config.

--- homes/joshk/mangohud/default.nix
+++ homes/joshk/mangohud/default.nix
@@ -1,8 +1,14 @@
 # ./mangohud/default.nix
+# You can test the config without a game running using the mesa-demos package
+#   Like v
 { pkgs, ... }:
 {
   programs.mangohud = {
     enable = true;
     enableSessionWide = true;
+    settings = import ./config.nix;
+    settingsPerApplication = {
+      # "steam" = ./steam.nix; # FIXME ? is this right idk
+    };
   };
 }
