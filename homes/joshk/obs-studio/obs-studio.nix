e5e781214910b2bdb91582554343ecba47d54471
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-05-06 17:14:26 -0600

    Reenabled obs and the obs background removal plugin

--- homes/joshk/obs-studio/obs-studio.nix
+++ homes/joshk/obs-studio/obs-studio.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 {
   programs.obs-studio = {
-    enable = false;
+    enable = true;
     plugins = [
-      # pkgs.obs-studio-plugins.obs-backgroundremoval
+      pkgs.obs-studio-plugins.obs-backgroundremoval
       # pkgs.obs-studio-plugins.obs-source-record
       # pkgs.obs-studio-plugins.input-overlay
     ];
