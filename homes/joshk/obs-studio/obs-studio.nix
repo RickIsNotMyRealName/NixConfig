ed8444e706e8ed40b0676a8d4d8537a5424ded57
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-05-08 08:54:38 -0600

    Enabled and set obs and its plugins to use unstable.

--- homes/joshk/obs-studio/obs-studio.nix
+++ homes/joshk/obs-studio/obs-studio.nix
@@ -2,10 +2,11 @@
 {
   programs.obs-studio = {
     enable = true;
+    package = pkgs.unstable.obs-studio;
     plugins = [
-      pkgs.obs-studio-plugins.obs-backgroundremoval
-      # pkgs.obs-studio-plugins.obs-source-record
-      # pkgs.obs-studio-plugins.input-overlay
+      pkgs.unstable.obs-studio-plugins.obs-backgroundremoval
+      pkgs.unstable.obs-studio-plugins.obs-source-record
+      pkgs.unstable.obs-studio-plugins.input-overlay
     ];
   };
 }
