d826639d764a811dc143ea8e26e20e90f9527ddf
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-05-09 21:40:53 -0600

    Fixed formatting.

--- homes/joshk/wm/hyprland/hyprland-conf.nix
+++ homes/joshk/wm/hyprland/hyprland-conf.nix
@@ -267,7 +267,7 @@
 
   monitor = [
     # DP-3 Left
-    "DP-3, highrr, 0x0, 1,vrr,1"
+    "DP-3, highrr, 0x0, 1, vrr, 1"
     # HDMI-A-1 Right 
     "HDMI-A-1, highrr, 1920x0, 1"
     "eDP-1, 1920x1080@144, 0x0, 1"
