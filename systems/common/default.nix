d02adc6b557bc474746888b908643d583ffa2662
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-05-09 21:44:40 -0600

    Added hyprland cachix.

--- systems/common/default.nix
+++ systems/common/default.nix
@@ -240,11 +240,13 @@
   security.polkit.enable = true;
 
   nix.settings = {
-    trusted-substituters = [
+    substituters = [
       "https://ai.cachix.org"
+      "https://hyprland.cachix.org"
     ];
     trusted-public-keys = [
       "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 
