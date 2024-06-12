bc62d3f2912180120de5f9511ccdeff6572976a1
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-06-11 18:39:10 -0600

    Switched `open-webui` to use the nixpkgs version instead of a docker container.

--- systems/NixOSDesktop/modules/open-webui.nix
+++ systems/NixOSDesktop/modules/open-webui.nix
@@ -1,36 +1,12 @@
-# https://fictionbecomesfact.com/nixos-ollama-oterm-openwebui
-{ config, ... }:
+{ pkgs, inputs, ... }:
 {
-  virtualisation = {
-    podman = {
-      enable = true;
-      dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
-    };
-    oci-containers = {
-      backend = "podman";
-      containers = {
-        "open-webui" = {
-          autoStart = false;
-          image = "ghcr.io/open-webui/open-webui";
-          ports = [ "3000:8080" ];
-          # TODO figure out how to create the data directory declaratively
-          volumes = [ "${config.users.users."joshk".home}/open-webui:/app/backend/data" ];
-          environment = {
-            OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
-            OLLAMA_BASE_URL = "http://127.0.0.1:11434";
-          };
-          extraOptions =
-            [
-              "--pull=newer"
-              "--name=open-webui"
-              "--hostname=open-webui"
-              "--network=host"
-              "--add-host=host.containers.internal:host-gateway"
-            ];
-        };
-      };
-    };
+  imports = [
+    "${inputs.nixpkgs-unstable}/nixos/modules/services/misc/open-webui.nix"
+  ];
+
+  services.open-webui = {
+    enable = true;
+    package = pkgs.unstable.open-webui;
+    openFirewall = true;
   };
-  networking.firewall = { allowedTCPPorts = [ 80 443 8080 11434 3000 ]; };
 }
