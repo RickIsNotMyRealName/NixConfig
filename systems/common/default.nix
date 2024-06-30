62d20504165a3e673bfff6d8c2f7726861902732
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-06-29 21:44:17 -0600

    Added the nixpkgs-python cachix.

--- systems/common/default.nix
+++ systems/common/default.nix
@@ -256,11 +256,13 @@
       "https://ai.cachix.org"
       "https://cuda-maintainers.cachix.org"
       "https://hyprland.cachix.org"
+      "https://nixpkgs-python.cachix.org"
     ];
     trusted-public-keys = [
       "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
     ];
   };
 
