357f55e94ed9020cadd9bf13932e4e6aa4dc0873
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-07-30 23:03:59 -0600

    Added josh_admin user config

--- config/nixos/users/josh_admin/default.nix
+++ config/nixos/users/josh_admin/default.nix
@@ -1,4 +1,20 @@
-{...}:
+{ pkgs, lib, ... }:
 {
-  
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.josh_admin = {
+    isNormalUser = true;
+    description = "josh_admin";
+    extraGroups = [ "networkmanager" "wheel" "video" "input" "adbusers" ];
+    shell = pkgs.zsh;
+    initialHashedPassword = "$y$j9T$jArB/OK7dwNWOYmxJ1Lm2/$BGA8dUMUqGp.lavPnys.JVdM.4J/7yeDdzUH4qqVw08"; # password is "test"
+  };
+
+  programs.nh = {
+    enable = lib.mkDefault true;
+    package = lib.mkDefault pkgs.unstable.nh;
+    clean.enable = lib.mkDefault true;
+    clean.extraArgs = lib.mkDefault "--keep-since 4d --keep 3";
+    flake = lib.mkDefault "/home/josh_admin/NixConfig";
+
+  };
 }
