3bd2c14696fdcc672821ce66e3e94077cbdb600f
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-08-03 16:10:55 -0600

    Added smb share to nix server config.

--- config/nixos/systems/NixOSServer/default.nix
+++ config/nixos/systems/NixOSServer/default.nix
@@ -2,7 +2,8 @@
 {
   imports = [
     # Use `profiles/minimal.nix` as the base configuration of this system but override `environment.noXlibs` to `false`.
-    (modulesPath + "/profiles/minimal.nix") { environment.noXlibs = false; }
+    (modulesPath + "/profiles/minimal.nix")
+    { environment.noXlibs = false; }
     ./hardware-configuration.nix
 
     # Traefik
@@ -15,6 +16,18 @@
     secrets = [
       "smb-secrets.env"
     ];
+  };
+
+  fileSystems."/mnt/NixConfig" = {
+    device = "//192.168.1.10/NixConfig";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in
+      [ "${automount_opts},credentials=/run/secrets/smb-secrets.env,uid=1000,gid=1000" ];
   };
 
   networking.hostName = "NixOSServer";
