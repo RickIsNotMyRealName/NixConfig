9ec4df5ebedd6c862ec44c87fce6f3ba747e41d3
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-08-09 23:41:32 -0600

    Added an automount for NAS ISOs share.

--- config/nixos/systems/NixOSDesktop/default.nix
+++ config/nixos/systems/NixOSDesktop/default.nix
@@ -159,6 +159,17 @@
       in
       [ "${automount_opts},credentials=/run/secrets/smb-secrets.env,uid=1000,gid=1000" ];
   };
+  fileSystems."/mnt/ISOs" = {
+    device = "//192.168.1.10/ISOs";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in
+      [ "${automount_opts},credentials=/run/secrets/smb-secrets.env,uid=1000,gid=1000" ];
+  };
 
   # Enable KDE Keep here for ease just in case hyprland breaks cause of nvidia things.
   services.xserver.enable = true;
