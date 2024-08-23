3b54b445a4d700db35bf9351ad79eb8af4a88b5f
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-08-22 18:32:23 -0600

    Attempt two at fixing build error with desktop

--- config/nixos/config/open-webui/default.nix
+++ config/nixos/config/open-webui/default.nix
@@ -1,12 +1,12 @@
 { pkgs, inputs, ... }:
 {
   imports = [
-    "${inputs.nixpkgs-pinned}/nixos/modules/services/misc/open-webui.nix"
+    # "${inputs.nixpkgs-pinned}/nixos/modules/services/misc/open-webui.nix"
   ];
 
   services.open-webui = {
     enable = true;
-    package = pkgs.pinned.open-webui;
+    package = pkgs.unstable.open-webui;
     openFirewall = true;
     host = "0.0.0.0";
   };
