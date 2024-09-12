b3d67d438f7b4c25abf8e2bf179d191a3a0fe232
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-12 18:24:40 -0400

    Added the configs for some of the mods into the config.

--- config/nixos/config/minecraft/default.nix
+++ config/nixos/config/minecraft/default.nix
@@ -1,26 +1,20 @@
 { pkgs, lib, inputs, ... }:
 {
-
   imports = [
     inputs.nix-minecraft.nixosModules.minecraft-servers
   ];
-
   nixpkgs.overlays = [
     inputs.nix-minecraft.overlay
   ];
-  
   networking.firewall.allowedUDPPorts = [ 24454 19132 ];
-
   services.minecraft-servers = {
     enable = true;
     eula = true;
     openFirewall = true;
-
     servers = {
       main = {
         enable = true;
         package = pkgs.fabricServers.fabric-1_21_1;
-
         serverProperties = {
           gamemode = "survival";
           difficulty = "normal";
@@ -28,13 +22,15 @@
           level-seed = "youngadults2024";
           server-port = 25565;
         };
-
         whitelist = {
           YeYeGame42069 = "38e9466c-26b8-44d3-b0c1-9ad456e0a156";
         };
-
         symlinks = {
           mods = ./servers/main/mods;
+          "config/Geyser-Fabric/config.yml" = ./servers/main/config/Geyser-Fabric/config.yml;
+          "config/fallingtree.json" = ./servers/main/config/fallingtree.json;
+          "config/ping-wheel.server.json" = ./servers/main/config/ping-wheel.server.json;
+          "config/voicechat/voicechat-server.properties" = ./servers/main/config/voicechat/voicechat-server.properties;
         };
       };
     };
