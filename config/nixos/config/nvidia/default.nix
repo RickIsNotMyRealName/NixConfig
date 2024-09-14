71bacdb2559ac10318ec9b1699b8ad47ea37862b
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-14 10:57:20 -0600

    Update nvidia driver to latest and cleaned up the file.

--- config/nixos/config/nvidia/default.nix
+++ config/nixos/config/nvidia/default.nix
@@ -1,20 +1,12 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 {
   # Hyprland Nvidia stuff
   environment.sessionVariables = {
-    # env = LIBVA_DRIVER_NAME,nvidia
-    # env = XDG_SESSION_TYPE,wayland
-    # env = GBM_BACKEND,nvidia-drm
-    # env = __GLX_VENDOR_LIBRARY_NAME,nvidia
-    # env = WLR_NO_HARDWARE_CURSORS,1
-
     "LIBVA_DRIVER_NAME" = "nvidia";
-    # "XDG_SESSION_TYPE" = "wayland";
     "GBM_BACKEND" = "nvidia-drm";
     "__GLX_VENDOR_LIBRARY_NAME" = "nvidia";
     "WLR_NO_HARDWARE_CURSORS" = "1";
   };
-
 
   # Enable OpenGL
   hardware.opengl = {
@@ -24,10 +16,9 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470 etc.
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
-
     # Modesetting is required.
     modesetting.enable = true;
 
@@ -55,25 +46,11 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "555.42.02";
-      sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-      sha256_aarch64 = "sha256-ekx0s0LRxxTBoqOzpcBhEKIj/JnuRCSSHjtwng9qAc0=";
-      openSha256 = "sha256-3/eI1VsBzuZ3Y6RZmt3Q5HrzI2saPTqUNs6zPh5zy6w=";
-      settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-      persistencedSha256 = "sha256-3ae31/egyMKpqtGEqgtikWcwMwfcqMv2K4MVFa70Bqs=";
-    };
+    # package = config.boot.kernelPackages.nvidiaPackages.latest;
+    package = pkgs.unstable.linuxPackages.nvidiaPackages.latest;
   };
-
-  # boot.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_drm" ];
-
-
-  #in `/etc/modprobe.d/nvidia.conf`
-  # "options nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3""
 
   boot.extraModprobeConfig = ''
     options nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3"
   '';
-
-
 }
