5e8d9751469cb1aed36871e4bd2b76745f5a9889
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-18 20:38:41 -0600

    Removed the iptables postSetup and postShutdown as wireguard seems to work without it and it was configured incorrectly anyways.

--- config/nixos/config/wireguard/default.nix
+++ config/nixos/config/wireguard/default.nix
@@ -31,17 +31,6 @@
         privateKeyFile = "/run/secrets/wireguard-server-key"; # Path to the server's private key
         listenPort = 51820;
 
-        # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-        # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-        postSetup = ''
-          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
-        '';
-
-        # This undoes the above command
-        postShutdown = ''
-          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
-        '';
-
         peers = [
           {
             publicKey = "bAtOGoLZX57W/ggInQL7d61v07beLNUYeXSYLbJ5oS0=";
