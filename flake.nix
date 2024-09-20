2bb331b9357e8eb51b3048c875ecc05d6989381e
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-20 17:46:10 -0600

    Fixed mismatched nixpkgs and home-manager version.

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
     };
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     spicetify-nix = {
