ddfcf674017ff8cf3bec68b1401e82e5d0b27307
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-20 21:44:52 -0600

    Formatted wireguard-client config file.

--- config/nixos/config/wireguard-client/default.nix
+++ config/nixos/config/wireguard-client/default.nix
@@ -21,7 +21,7 @@
         dns = [ "192.168.1.12" "1.0.0.1" "1.1.1.1" ];
         peers = [{
           publicKey = "zAwr/adosi1wxUQIsnU/hn/jRgwGcAZGZZRIRg907lc=";
-          allowedIPs = [ "0.0.0.0/0" "192.168.1.0/24"];
+          allowedIPs = [ "0.0.0.0/0" "192.168.1.0/24" ];
           endpoint = "tden.xyz:51820";
           persistentKeepalive = 25;
         }];
