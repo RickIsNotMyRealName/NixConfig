a93e7a0a3d9f0c20b53ddcde8f93fe373c9ed1a9
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-22 23:01:39 -0600

    Rearranged cachix entries.

--- config/nixos/config/cachix/default.nix
+++ config/nixos/config/cachix/default.nix
@@ -2,21 +2,21 @@
 {
   nix.settings = {
     trusted-public-keys = [
-      "rickisnotmyrealname.cachix.org-1:livOzeSQwWDD2XUFIspxkMCcu7Pi+angYDoLajI6T/k="
-      "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+      "rickisnotmyrealname.cachix.org-1:livOzeSQwWDD2XUFIspxkMCcu7Pi+angYDoLajI6T/k="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
     ];
     
     substituters = [
-      "https://ai.cachix.org"
-      "https://cuda-maintainers.cachix.org"
-      "https://hyprland.cachix.org"
       "https://nixpkgs-python.cachix.org"
       "https://rickisnotmyrealname.cachix.org"
       "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+      "https://ai.cachix.org"
     ];
   };
 }
