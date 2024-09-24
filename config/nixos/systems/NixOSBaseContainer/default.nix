18b78b9dd579e1f26aa0b045f94d171128d44728
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-23 18:38:40 -0600

    Added ssh keys for initial build.

--- config/nixos/systems/NixOSBaseContainer/default.nix
+++ config/nixos/systems/NixOSBaseContainer/default.nix
@@ -5,4 +5,15 @@
   environment.systemPackages = with pkgs; [
     git
   ];
+
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPj8gjwywhMlsCqD/2v5d1ba6RbbW4QUo9sk+UkokyhW root@nixos"
+    ];
+  };
+
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "yes";
+  services.openssh.passwordAuthentication = true;
+  
 }
