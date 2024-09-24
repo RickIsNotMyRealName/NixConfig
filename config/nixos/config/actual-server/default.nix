ae3f007ecc3c6b9d5a2044fe18688bcefa550e0c
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-23 22:51:28 -0600

    Updated actual-server config too match the old servers configuration.

--- config/nixos/config/actual-server/default.nix
+++ config/nixos/config/actual-server/default.nix
@@ -1,6 +1,20 @@
-{...}:
+# Note to self: The selfsigned certificate will need to be regenerated in 365 days.
+# Today is: Sept 23, 2024
+# Symptoms: Syncing errors.
+{ ... }:
 {
   services.actual-server = {
     enable = true;
+    hostname = "::";
+    openFirewall = true;
+    https = {
+      "key" = "/var/lib/actual-server/certs/selfhost.key";
+      "cert" = "/var/lib/actual-server/certs/selfhost.crt";
+    };
+    upload = {
+      fileSizeLimitMB = "200";
+      syncEncryptedFileSizeLimitMB = "200";
+      fileSizeSyncLimitMB = "200";
+    };
   };
 }
