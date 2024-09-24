06532cab78c1563237f670f1b3ceb703d321160c
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-24 16:26:14 -0600

    Added adminpassFile option for nextcloud.

--- config/nixos/config/nextcloud/default.nix
+++ config/nixos/config/nextcloud/default.nix
@@ -1,8 +1,17 @@
 { pkgs, ... }:
 {
+  myConfig = {
+    secrets = [
+      "adminpassFile"
+    ];
+  };
+
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud29;
     hostName = "nextcloud.tden.xyz";
+    config = {
+      adminpassFile = "/run/secrets/adminpassFile";
+    };
   };
 }
