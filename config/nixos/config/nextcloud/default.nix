07a77eafaf3e6ace22b0ffe4e31093e254276bab
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-25 18:38:39 -0600

    Nextcloud is now working.

--- config/nixos/config/nextcloud/default.nix
+++ config/nixos/config/nextcloud/default.nix
@@ -8,12 +8,27 @@
     };
   };
 
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 80 ];
+  };
+
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud29;
-    hostName = "nextcloud.tden.xyz";
+    hostName = "cloud.tden.xyz";
+    home = "/mnt/DataDrive/";
+    datadir = "/mnt/DataDrive/";
     config = {
       adminpassFile = "/run/secrets/adminpassFile";
+      adminuser = "admin";
     };
+    settings = {
+      trusted_domains = [ "localhost" "192.168.1.19" "cloud.tden.xyz" "192.168.1.22" ];
+      trusted_proxies = [ "192.168.1.19" "192.168.1.22" ];
+      overwriteprotocol = "https";
+    };
+    database.createLocally = true;
+    configureRedis = true;
   };
 }
