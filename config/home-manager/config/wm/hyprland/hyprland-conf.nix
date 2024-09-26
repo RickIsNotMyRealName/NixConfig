9cf43b37b13b7f9c1401ed2beb80fe39db4eee22
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-26 17:10:36 -0600

    Added some more hyprland animations.

--- config/home-manager/config/wm/hyprland/hyprland-conf.nix
+++ config/home-manager/config/wm/hyprland/hyprland-conf.nix
@@ -409,10 +409,16 @@
 
   bezier = [
     "overshoot, 0.5, 1.5, 0.5, 1.5"
+    "myBezier, 0.05, 0.9, 0.1, 1.05"
   ];
 
   animation = [
     "workspaces, 1, 3, overshoot"
+    "windows, 1, 7, myBezier"
+    "windowsOut, 1, 7, default, popin 80%"
+    "border, 1, 10, default"
+    "borderangle, 1, 8, default"
+    "fade, 1, 7, default"
   ];
 
   windowrulev2 = [
