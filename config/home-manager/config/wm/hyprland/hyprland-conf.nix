cba0f12712b735a1aabeeca0c5fbb77d9d6f6b2a
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-09-27 11:41:40 -0600

    Added `windowsIn` animation to hyprland

--- config/home-manager/config/wm/hyprland/hyprland-conf.nix
+++ config/home-manager/config/wm/hyprland/hyprland-conf.nix
@@ -419,6 +419,7 @@
     "workspaces, 1, 3, overshoot"
     "windows, 1, 7, myBezier"
     "windowsOut, 1, 7, default, popin 80%"
+    "windowsIn, 1, 7, default, popin 80%"
     "border, 1, 10, default"
     "borderangle, 1, 8, default"
     "fade, 1, 7, default"
