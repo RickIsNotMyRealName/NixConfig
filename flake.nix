c65abc41e9280c6c045742aa49e22d3437e4eac5
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2024-10-17 18:40:44 -0600

    Added my personal cachix to `extra-substituters`.

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
 
   nixConfig = {
     extra-substituters = [
+      "rickisnotmyrealname.cachix.org"
       "https://ai.cachix.org"
       "https://cuda-maintainers.cachix.org"
       "https://hyprland.cachix.org"
       "https://nixpkgs-python.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "rickisnotmyrealname.cachix.org-1:livOzeSQwWDD2XUFIspxkMCcu7Pi+angYDoLajI6T/k="
       "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
